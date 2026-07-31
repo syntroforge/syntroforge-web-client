dec60e22765d765ca6d47bbd24ddd3107bce0560
Martin Dickopp <martin@syntroforge.io>
2026-07-31 18:14:01 +0200

    Shorter asset directory name

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { dirname } from "node:path";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
-/** Root directory, i.e., the directory containing this file. */
-const ROOT_DIR = dirname(new URL(import.meta.url).pathname);
-
 export default defineConfig({
+  build: {
+    assetsDir: "a",
+    reportCompressedSize: false,
+  },
+  clearScreen: false,
   plugins: [solidPlugin()],
-  resolve: { alias: { "~": `${ROOT_DIR}/src` } },
+  resolve: { tsconfigPaths: true },
 });

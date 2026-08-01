98af3890f2136979f50bedfb2c67d2bb204fec18
Martin Dickopp <martin@syntroforge.io>
2026-08-01 13:33:52 +0200

    Configure oxlint

---
+++ oxlint.config.ts
@@ -0,0 +1,28 @@
+import solid from "eslint-plugin-solid/configs/typescript";
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  categories: { correctness: "error" },
+  env: {},
+  options: { typeAware: true, typeCheck: true },
+  plugins: ["eslint", "typescript", "unicorn", "oxc", "import", "jsdoc", "promise"],
+  overrides: [
+    {
+      files: ["src/**"],
+      env: {
+        es2023: true,
+        browser: true,
+      },
+      jsPlugins: ["eslint-plugin-solid"],
+      plugins: ["jsx-a11y"],
+      rules: { ...solid.rules },
+    },
+    {
+      files: ["*", "build/**"],
+      env: {
+        es2025: true,
+        node: true,
+      },
+    },
+  ],
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ParentComponent } from "solid-js";
 
 const Layout: ParentComponent = (props) => {
+  // oxlint-disable-next-line solid/reactivity
   return props.children;
 };
 
